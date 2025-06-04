534a3ae27100fcdccd79f213910efc380a8d41fb
Georgios Giaslas <ggiaslas@trading-point.com>
2025-06-04 08:40:51 +0400

    use monotonic for epoch clock

--- aeron-client/src/main/c/util/aeron_clock.c
+++ aeron-client/src/main/c/util/aeron_clock.c
@@ -124,7 +124,7 @@ int64_t aeron_nano_clock(void)
 int64_t aeron_epoch_clock(void)
 {
     struct timespec ts;
-    if (aeron_clock_gettime_realtime_coarse(&ts) < 0)
+    if (aeron_clock_gettime_monotonic(&ts) < 0)
     {
         return -1;
     }
